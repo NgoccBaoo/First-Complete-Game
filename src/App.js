ae78bc5269bad18ee01e340dee77e0bbf4e51f00
NgoccBaoo <baozane17@gmail.com>
2022-04-26 21:19:09 +0700

    first upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import GameEvent from './Modules/GameEvent';
+
+function App() {
+  return (
+    <div>
+      <GameEvent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Modules/GameEvent.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+
+class GameEvent extends Component {
+    constructor(props) {
+        super (props);
+        this.state = {
+            speed: 1,
+            position_road: 0,
+            time_run: 0,
+            score: 0,
+            player_position_x: 0,
+            player_position_y: 0,
+            player_position_deg: 0,
+        }
+        this.handleControler = this.handleControler.bind(this);
+    }
+
+    componentDidMount () {
+        this.intervalId = setInterval(this.run_game.bind(this), 40); 
+        let input = document.getElementById('dieu_khien');
+        input.focus();
+    }
+
+    run_game = () => {
+        //console.log(this.state.speed);
+        this.setState({
+            speed: 1,
+            position_road: this.state.position_road + this.state.speed,
+            time_run: this.state.time_run + 40,
+            score: this.state.score + this.state.speed
+        });
+    }
+
+    handleControler = (e) => {
+        console.log (e.keyCode);
+
+        if(e.keyCode === 37) //trai 
+        {
+            if(this.state.player_position_y >=403)
+            {
+                //do nothing
+            }
+            else {
+                this.setState(prevState => {
+                    prevState.player_position_x -= 1;
+                    prevState.player_position_deg = -5;
+                    return prevState;
+                })
+            }
+           
+        }
+        else if (e.keyCode === 38) //tren
+        {
+            if(this.state.player_position_x >=211)
+            {
+                //do nothing
+            }
+            else {
+                this.setState(prevState => {
+                    prevState.player_position_y += 1;
+                    return prevState;
+                })
+            }
+            
+        }
+        else if (e.keyCode === 39) //phai
+        {
+            if(this.state.player_position_x >=211)
+            {
+                //do nothing
+            }
+            else {
+                this.setState(prevState => {
+                    prevState.player_position_x += 1;
+                    prevState.player_position_deg = 5;
+                    return prevState;
+                })
+            }
+            
+        }
+        else if (e.keyCode === 40) //duoi
+        {
+            if(this.state.player_position_y <0)
+            {
+                //do nothing
+            }
+            else {
+                this.setState(prevState => {
+                    prevState.player_position_y -= 1;
+                    return prevState;
+                })
+            }
+            
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='game_container' style={{backgroundPosition: '0' + this.state.position_road + "px"
+                }}>
+                    <div className='score'>{this.state.score}</div>
+                    <div className='player'></div>
+
+                </div>
+                <input type="text" className='dieu_khien' id='dieu_khien'/>
+            </div>
+        );
+    }
+}
+
+export default GameEvent;
